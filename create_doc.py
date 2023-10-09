fba767d28d4dd33e1d64aca1bd655aab2b9fc585
Jack <jackbullen@icloud.com>
2023-10-09 13:56:48 -0700

    generate custom cover letter

---
+++ create_doc.py
@@ -0,0 +1,21 @@
+import os
+
+with open('./latex_template.txt', 'r') as f:
+    LATEX = f.read()
+
+for _,_,files in os.walk('./modified_cover_letters'):
+    for file in files:
+        OUTTEX = LATEX.split('--!!--')[0]
+
+        with open(f'./modified_cover_letters/{file}', 'r') as f:
+            content = f.read()
+
+            OUTTEX +=  ('\n\n'+r'\vspace{1em}'+'\n\n').join('\n\n'.join([x for x in content.split('Dear')[0].split('\n\n')]).split('\n\n'))
+
+            for i, paragraph in enumerate(content.split('Dear')[1].split('\n')):
+                if i == 0:
+                    paragraph = 'Dear ' + paragraph
+                OUTTEX += r'\noindent' + '\n ' + paragraph + '\n' + r'\vspace{1em}'
+
+        with open('./docs/cover_letter_' + file.split('.')[0] + '.tex', 'w') as f:
+            f.write(OUTTEX+r'\end{document}')
